2993fc012417b5ebd5a46f517a8297a3eaf8cb03
@Ceo-Sammarco <noah.kd@gmail.com>
2024-01-23 17:00:55 -0300

    @Ceo-Sammarco - Tue 23 Jan 2024

---
+++ src/parcelas.js
@@ -0,0 +1,132 @@
+
+
+let 
+   acordo = {
+      ano1: {
+         id: 1,
+         ano: "2023",
+         basePath: "./parcelamento/1° ano - prcls 01 - 05/",
+         parcelas: [
+            p1 = {
+               id: "1",
+               boleto: "iptu parcela 01.pdf",
+               comprovante: "iptu comprovante 01.jpg",
+               status: "1",
+            },
+            p2 = {
+               id: "2",
+               boleto: "iptu parcela 02.pdf",
+               comprovante: "iptu comprovante 02.jpg",
+               status: "1",
+            },
+            p3 = {
+               id: "3",
+               boleto: "iptu parcela 03.pdf",
+               comprovante: "iptu comprovante 03.jpg",
+               status: "1",
+            },
+            p4 = {
+               id: "4",
+               boleto: "iptu parcela 04.pdf",
+               comprovante: "iptu comprovante 04.png",
+               status: "1",
+            },
+            p5 = {
+               id: "5",
+               boleto: "iptu parcela 05.pdf",
+               comprovante: "iptu comprovante 05.png",
+               status: "1",
+            },
+         ]
+      },
+      
+      ano2: {
+         id: 2,
+         ano: "2024",
+         basePath: "./parcelamento/2° ano - prcls 06 - 17/",
+         parcelas: [
+            p6 = {
+               id: "6",
+               boleto: "p6 24-01-2024.pdf",
+               comprovante: "p6 comprovante.jpg",
+               status: "1",
+            },
+            p7 = {
+               id: "7",
+               boleto: "p7 24-02-2024",
+               comprovante: "",
+               status: "",
+            },
+            p8 = {
+               id: "8",
+               boleto: "p8 24-03-2024",
+               comprovante: "",
+               status: "",
+            },
+            p9 = {
+               id: "9",
+               boleto: "p9 24-04-2024",
+               comprovante: "",
+               status: "",
+            },
+            p10 = {
+               id: "10",
+               boleto: "p10 24-05-2024",
+               comprovante: "",
+               status: "",
+            },
+            p11 = {
+               id: "11",
+               boleto: "p11 24-06-2024",
+               comprovante: "",
+               status: "",
+            },
+            p12 = {
+               id: "12",
+               boleto: "p12 24-07-2024",
+               comprovante: "",
+               status: "",
+            },
+            p13 = {
+               id: "13",
+               boleto: "p13 24-08-2024",
+               comprovante: "",
+               status: "",
+            },
+            p14 = {
+               id: "14",
+               boleto: "p14 24-09-2024",
+               comprovante: "",
+               status: "",
+            },
+            p15 = {
+               id: "15",
+               boleto: "p15 24-10-2024",
+               comprovante: "",
+               status: "",
+            },
+            p16 = {
+               id: "16",
+               boleto: "p16 24-11-2024",
+               comprovante: "",
+               status: "",
+            },
+            p17 = {
+               id: "17",
+               boleto: "p17 24-12-2024",
+               comprovante: "",
+               status: "",
+            },
+         ]
+      }
+   }
+;
+
+/* 
+   p1 = {
+      boleto: "",
+      id: "",
+      comprovante: "",
+      status: "",
+   }
+*/
--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 
 
 const 
-   $ = v => document.querySelector( v ),
-   $$ = v => document.querySelectorAll( v ),
    _ = ( ...args ) => console.log( ...args ),
-   _e = ( ...args ) => console.error( ...args )
+   _e = ( ...args ) => console.error( ...args ),
+   $ = v => document.querySelector( v ),
+   $$ = v => document.querySelectorAll( v )
 ;
 
 window.addEventListener( "load", () => {
@@ -91,5 +91,176 @@ window.addEventListener( "load", () => {
       drawer.style.visibility = "hidden";
       drawer.style.width = "0";
    } );
+
+
+   /* == [ card-item header class ]
+   == == == == == == == == == */
+   const 
+      CIHeaderTemplate = `
+      <style>
+         titulo{
+            display: grid;
+            grid-template-columns: repeat( auto-fit, minmax( 100px, max-content ) );
+            width: 100%;
+            justify-content: center;
+            align-items: center;
+         }
+         titulo :first-child {
+            font-size: 1.5rem;
+            font-weight: bold;
+         }
+         p {
+            display: grid;
+            place-items: center;
+            width: 100%;
+            height: 100%;
+            padding: 0 2px;
+            background: #ff7a18;
+            font-weight: 900;
+            color: #fff;
+         }
+      </style>
+      <titulo>
+         <t></t>
+         <t></t>
+      </titulo>
+      <p></p>
+      `
+   ;
+   class PageHeader extends HTMLElement {
+      constructor() {
+         super();
+
+         let 
+            element = document.createElement( "template" )
+         ;
+
+         element.innerHTML = CIHeaderTemplate;
+
+         this.attachShadow( { mode: "open" } );
+         this.shadowRoot.append( 
+            element.content.cloneNode( true )
+         );
+
+         this.style.background = this.getAttribute( "bg" );
+         _( this.querySelector( "p" ) );
+         this.shadowRoot.querySelector( "p" ).innerText = this.getAttribute( "ano" );
+         this.shadowRoot.querySelector( "titulo :first-child" ).innerHTML = this.getAttribute( "t1" );
+         this.shadowRoot.querySelector( "titulo :last-child" ).innerHTML = this.getAttribute( "t2" );
+
+      }
+   }
+   window.customElements.define( "page-header", PageHeader );
+
+   /* == [ card-item class ]
+   == == == == == == == == == */
+   const 
+      CITemplate = `
+         <style>
+            h2 { color: #0089c7; }
+            [boleto] { height: 150px; }
+            [boleto] > img { height: 100%; }
+            [comprovante] { width: 150px; }
+            [comprovante] > img { width: 100%; }
+         </style>
+         <h2></h2>
+         <navlink src="" boleto>
+            <img src="./src/boleto.png" alt="">
+         </navlink>
+         <navlink src="" comprovante>
+            <img src="./src/comprovante.png" alt="">
+         </navlink>
+      `
+   ;
+   
+   class CardItem extends HTMLElement {
+      constructor() {
+         super();
+
+         let 
+            element = document.createElement( "template" )
+         ;
+
+         element.innerHTML = CITemplate;
+
+         this.attachShadow( { mode: "open" } );
+         this.shadowRoot.append( 
+            element.content.cloneNode( true )
+         );
+         
+         this.shadowRoot.querySelector( "h2" ).innerHTML = `
+            Parcela ${ this.getAttribute( "idn" ) }
+         `;
+         
+         this.shadowRoot.querySelector( "navlink[boleto]" ).setAttribute( 
+            "src", 
+            `${ this.getAttribute( "boleto" ) }`
+         );
+         
+         this.shadowRoot.querySelector( "navlink[comprovante]" ).setAttribute( 
+            "src", 
+            `${ this.getAttribute( "comprovante" ) }`
+         );
+
+         this.getAttribute( "status" ) != "1" ? 
+            _( "" ) : this.setAttribute( "pago", "" );
+            
+         this.shadowRoot.querySelectorAll( "navlink" ).forEach( link => link.addEventListener( "click", ev => {
+            window.open( link.getAttribute( "src" ), "_blank" );
+         } ) );
+      }
+   }
+   window.customElements.define( "card-item", CardItem );
+
+   for( i in acordo ) {
+      let 
+         elemento = document.createElement( `ano${ acordo[i].id }` )
+      ;
+      
+      document.querySelector( "main" ).innerHTML +=
+         `<ano${ acordo[i].id } card id="ano${ acordo[i].id }">
+            <page-header 
+               bg="#fff2df"
+               t1="${ acordo[i].id }° ano"
+               t2="parcelas: 
+                  ${   
+   acordo[i].ano == "2023" ? "1 - 5 / 120" 
+      : acordo[i].ano == "2024" ? "6 - 17 / 120" 
+         : acordo[i].ano == "2025" ? "18 - 29 / 120" 
+            : acordo[i].ano == "2026" ? "29 - 41 / 120" 
+               : acordo[i].ano == "2027" ? "41 - 53 / 120" 
+                  : acordo[i].ano == "2028" ? "53 - 65 / 120" 
+                     : acordo[i].ano == "2029" ? "65 - 77 / 120" 
+                        : acordo[i].ano == "2030" ? "77 - 89 / 120" 
+                           : acordo[i].ano == "2031" ? "89 - 101 / 120" 
+                              : acordo[i].ano == "2032" ? "101 - 113 / 120" 
+                                 : "113 - 120 / 120" 
+}
+               "
+               ano="${ acordo[i].ano }"
+            >
+            </page-header>
+            <home>
+               <parcelas></parcelas>
+            </home>
+         </ano${ acordo[i].id }>   
+         `;
+   }
+
+   for( i in acordo ) {
+
+      acordo[i].parcelas.forEach( parcela => {
+         document.querySelector( `ano${ acordo[i].id } > home > parcelas` ).innerHTML += `
+            <card-item 
+               idn="${ parcela.id }"
+               boleto="${ acordo[i].basePath }${ parcela.boleto }"
+               comprovante="${ acordo[i].basePath }${ parcela.comprovante }"
+               status="${ parcela.status }"
+            >
+            </card-item>
+         `;
+      } );
+
+   }
 
 } );
